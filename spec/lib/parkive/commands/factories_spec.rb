4378f0bbbdc278d7020d6189b35c1bd3069f8c68
Davis W. Frank <dwfrank@gmail.com>
2024-11-08 08:24:58 -0800

    Cleanup of strings

--- spec/lib/parkive/commands/factories_spec.rb
+++ spec/lib/parkive/commands/factories_spec.rb
@@ -2,14 +2,14 @@ module Parkive::Commands
   RSpec.describe Factory do
     describe ".forMakeDirectories" do
       context "when gum is not installed" do
-        it "returns a MakeDirectories" do
+        it "returns the right class" do
           klass = Factory.forMakeDirectories(false)
           expect(klass).to eq(MakeDirectories)
         end
       end
 
-      context "when gum is  installed" do
-        it "returns a MakeDirectories" do
+      context "when gum is installed" do
+        it "returns the right class" do
           klass = Factory.forMakeDirectories(true)
           expect(klass).to eq(GumMakeDirectories)
         end
