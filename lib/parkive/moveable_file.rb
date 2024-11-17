9f3aad01a266eb77a4610d3f101f59cfeefef965
Davis W. Frank <dwfrank@gmail.com>
2024-11-17 11:03:08 -0800

    Adds MoveableFile#exist?

--- lib/parkive/moveable_file.rb
+++ lib/parkive/moveable_file.rb
@@ -3,13 +3,19 @@
 module Parkive
   class MoveableFile
     attr_accessor :move
+    attr_reader :path
 
-    def initialize
+    def initialize(path)
       @move = false
+      @path = path
     end
 
     def move?
       move
+    end
+
+    def exist?
+      File.exist?(path)
     end
   end
 end
--- spec/lib/parkive/moveable_file_spec.rb
+++ spec/lib/parkive/moveable_file_spec.rb
@@ -2,22 +2,48 @@
 
 module Parkive
   RSpec.describe MoveableFile do
-
-    let(:mfile) { MoveableFile.new }
-
-    describe "#move" do
-      it "is an attribute" do
-        expect(mfile.move).to eq(false)
-        mfile.move = true
-        expect(mfile.move).to eq(true)
+    around :each do |example|
+      Dir.mktmpdir do |dir|
+        @temp_dir = dir
+        example.run
       end
     end
 
-    describe "#move?" do
-      it "reflects the state of the attribute" do
-        expect(mfile.move?).to eq(false)
-        mfile.move = true
-        expect(mfile.move?).to eq(true)
+    let(:temp_dir) { @temp_dir }
+    let(:path) { "#{temp_dir}/test.txt" }
+    let(:mfile) { MoveableFile.new(path) }
+
+    describe "#path" do
+      it "should be an attribute" do
+        expect(mfile.path).to eq(path)
+      end
+    end
+
+    context "existence" do
+      describe "#exist?" do
+        it "should reflect the path on the filesystem" do
+          expect(mfile.exist?).to eq(false)
+          FileUtils.touch(path)
+          expect(mfile.exist?).to eq(true)
+        end
+      end
+    end
+
+    context "wanting to move" do
+      describe "#move" do
+        it "is an attribute" do
+          expect(mfile.move).to eq(false)
+          mfile.move = true
+          expect(mfile.move).to eq(true)
+        end
+      end
+
+      describe "#move?" do
+        it "reflects the state of the attribute" do
+          expect(mfile.move?).to eq(false)
+          mfile.move = true
+          expect(mfile.move?).to eq(true)
+        end
       end
     end
   end
