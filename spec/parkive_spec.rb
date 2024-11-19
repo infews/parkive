5f5c5dcb3a07f3e2f8ce77a92b4d5f9561b7b9b0
Davis W. Frank <dwfrank@gmail.com>
2024-11-18 17:33:19 -0800

    Removes useless spec

--- spec/parkive_spec.rb
+++ spec/parkive_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Parkive do
   it "has a version number" do
     expect(Parkive::VERSION).not_to be nil
   end
-
-  xit "does something useful" do
-    expect(false).to eq(true)
-  end
 end
