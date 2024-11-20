cd24c31e54391fa7ba060a327f603698bccd630e
Davis W. Frank <dwfrank@gmail.com>
2024-11-20 11:33:10 -0800

    Cleaning up custom errors

--- lib/parkive.rb
+++ lib/parkive.rb
@@ -7,7 +7,8 @@ require_relative "cli"
 module Parkive
   class NoDestinationDirectoryError < Thor::Error
     def initialize(path)
-      super("Destination directory \"#{path}\" does not exist.")
+      message = "Destination directory \"#{path}\" does not exist."
+      super(message)
     end
   end
 
